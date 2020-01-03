06f0347f7155b643988ddced1c61f740dc339385
kyledennison <kyledennison090@gmail.com>
2020-01-03 16:19:23 -0500

    When a label is dropped into the text area it transfers the text plus the curly brackets. This was done using just the Transferable object.

--- src/main/java/DD.java
+++ src/main/java/DD.java
@@ -1,35 +1,107 @@
 import com.intellij.openapi.wm.ToolWindow;
 
 import javax.swing.*;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.dnd.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.IOException;
 
 
 public class DD {
     private JPanel panel1;
     private JPanel dropArea;
     private JPanel dragArea;
+
     private JLabel ifLabel;
-    private JButton btn;
-    private JButton btn2;
+    private JLabel elseLabel;
+    private JTextArea textArea1;
+
 
 
     public DD(ToolWindow toolWindow) {
-        ifLabel.setTransferHandler(new TransferHandler("text"));
+        TransferHandler dnd = new TransferHandler(){
+            @Override
+            public boolean canImport(TransferSupport support) {
+                if (!support.isDrop()) {
+                    return false;
+                }
+                //only Strings
+                if (!support.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+                    return false;
+                }
+                return true;
+            }
+            @Override
+            public boolean importData(TransferSupport support) {
+                if (!canImport(support)) {
+                    return false;
+                }
+
+                Transferable tansferable = support.getTransferable();
+                String line;
+                try {
+                    line = (String) tansferable.getTransferData(DataFlavor.stringFlavor);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return false;
+                }
+                JTextArea.DropLocation dl = (JTextArea.DropLocation) support.getDropLocation();
+                JTextArea a = (JTextArea) support.getComponent();
+                a.setText(line);
+                //textArea1.setText(line);
+                return true;
+            }
+        };
+
+        textArea1.setDragEnabled(true);
+        textArea1.setTransferHandler(dnd);
+
+        MyDragGestureListener dlistener = new MyDragGestureListener();
+        DragSource ds1 = new DragSource();
+        ds1.createDefaultDragGestureRecognizer(ifLabel, DnDConstants.ACTION_COPY, dlistener);
+
+        //ifLabel.setTransferHandler(new TransferHandler("text"));
+        elseLabel.setTransferHandler(new TransferHandler("text"));
+
         MouseListener ml = new MouseAdapter() {
+            @Override
             public void mousePressed(MouseEvent e) {
+                //System.out.println("test2");
                 JComponent c = (JComponent)e.getSource();
                 TransferHandler th = c.getTransferHandler();
                 th.exportAsDrag(c, e, TransferHandler.COPY);
             }
         };
-        ifLabel.addMouseListener(ml);
-           btn.setTransferHandler(new TransferHandler("text"));
-        btn2.setTransferHandler(new TransferHandler("text"));
+
+
+
+
+        //ifLabel.addMouseListener(ml);
+        elseLabel.addMouseListener(ml);
+        //textArea1.addMouseListener(ml2);
+        //DropTarget dt = new DropTarget(textArea1,d);
+
+
+
     }
+
+
 
     public JPanel getContent() {
         return panel1;
     }
 }
+
+/*
+        DropTargetListener d = new DropTargetAdapter() {
+            @Override
+            public void drop(DropTargetDropEvent dtde) {
+
+
+            }
+        };
+        */
